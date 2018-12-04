15c5b3d76c6c9a339e53466e55cbe726e8bd2fe3
Vequan Clark <vclark@DEGW01VCLARK.local>
2018-12-04 17:20:55 -0600

    fixed webpack resolve issue

--- webpack.config.js
+++ webpack.config.js
@@ -28,5 +28,8 @@ module.exports = {
         rules: [
           { test: /\.(js)$/, exclude: /node_modules/, loader: 'babel-loader' }
         ]
+      },
+      resolve: {
+          extensions: ['.js']
       }
 };
