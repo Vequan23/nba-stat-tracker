5f9c01f969fe858247ee65a7de9e582afd5207cc
Vequan Clark <vclark@DEGW01VCLARK.local>
2018-12-05 11:57:24 -0600

    configured eslint

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,42 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: './src/js/app.js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        port: 8001
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './index.html',
-            chunks: ['app']
-        }),
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-          { test: /\.(js)$/, exclude: /node_modules/, loader: 'babel-loader' }
-        ]
+  mode: 'development',
+  entry: {
+    app: './src/js/app.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 8005,
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+      chunks: ['app'],
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
       },
-      resolve: {
-          extensions: ['.js']
-      }
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
 };
